9ef67e44498756299a04baabb9e92e8866e45f39
Chad Anderson <chad@designerchad.com>
2018-08-10 22:01:39 -0700

    Timer working

--- OTA-light-timer/OTA-light-timer.ino
+++ OTA-light-timer/OTA-light-timer.ino
@@ -14,12 +14,29 @@
 #include <WiFiUdp.h>
 #include <ArduinoOTA.h>
 
+#include "/Users/chadwickanderson/documents/arduino/Includes/networkheader"
+
 // ###### OTA #################################
 // Replace with your network credentials
-const char* ssid = "*****";
-const char* password = "*****";
-const int ESP_BUILTIN_LED = 2;
+//const char* ssid = "*****";
+//const char* password = "*****";
+//const int ESP_BUILTIN_LED = 2;
 // ########################################
+
+// ###### WEBSERVER SETUP #################################
+// Set web server port number to 80
+WiFiServer server(80);
+// Variable to store the HTTP request
+String header;
+// Auxiliar variables to store the current output state
+String output5State = "off";
+String output4State = "off";
+
+// Assign output variables to GPIO pins
+const int output5 = 5;
+const int output4 = 4;
+
+
 
 // ###### TIME STUFF #################################
 // all from Horack
@@ -35,7 +52,7 @@ int nptAttempts = 0;
 #define interval 5000
 
 #include "FastLED.h"
-#define NUM_LEDS 40 //155
+#define NUM_LEDS 22 //155
 
 struct CRGB leds3[NUM_LEDS];
 struct CRGB leds4[NUM_LEDS];
@@ -49,7 +66,9 @@ uint8_t gBrightness = 180;
 // ########################################
 
 void setup() {
-
+  //fill_solid( leds6, NUM_LEDS, CHSV(0,0,0));
+  FastLED.clear();
+  //digitalWrite(6, LOW);
 // ###### OTA ESSENTIALS ######################################  
   Serial.begin(115200);
   Serial.println("Booting");
@@ -95,6 +114,16 @@ void setup() {
 
 // ######  / OTA ESSENTIALS ###################################### 
 
+  
+  // Initialize the output variables as outputs
+  pinMode(output5, OUTPUT);
+  pinMode(output4, OUTPUT);
+  // Set outputs to LOW
+  digitalWrite(output5, LOW);
+  digitalWrite(output4, LOW);
+
+
+
 
 // #### LED CODE BEGIN #######
     FastLED.addLeds<WS2812B,3,GRB>(leds3, NUM_LEDS).setCorrection(TypicalLEDStrip); // TALL ARM
@@ -102,7 +131,9 @@ void setup() {
     FastLED.addLeds<WS2812B,5,GRB>(leds5, NUM_LEDS).setCorrection(TypicalLEDStrip); // TALL ARM
     FastLED.addLeds<WS2812B,6,GRB>(leds6, NUM_LEDS).setCorrection(TypicalLEDStrip); // TALL ARM
 
-  
+  //Webserver startup
+  server.begin();
+
 } //setup
 
 uint8_t gHue = 0; // rotating "base color" used by many of the patterns
@@ -113,8 +144,130 @@ void loop() {
 
   updateTimeFromServer(); // HORACK - sync our internal clock periodically with real time from remote server (also gets actual time rather than relative from boot time)
 
-
+// #### WEBSERVER / CLIENT #################################################
   
+WiFiClient client = server.available();   // Listen for incoming clients
+  //delay(15000); // delay to give the NTP server time to fetch correct time
+
+  if (client) {                             // If a new client connects,
+    Serial.println("New Client.");          // print a message out in the serial port
+    String currentLine = "";                // make a String to hold incoming data from the client
+    while (client.connected()) {            // loop while the client's connected
+      if (client.available()) {             // if there's bytes to read from the client,
+        char c = client.read();             // read a byte, then
+        Serial.write(c);                    // print it out the serial monitor
+        header += c;
+        if (c == '\n') {                    // if the byte is a newline character
+          // if the current line is blank, you got two newline characters in a row.
+          // that's the end of the client HTTP request, so send a response:
+          if (currentLine.length() == 0) {
+            // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
+            // and a content-type so the client knows what's coming, then a blank line:
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println("Connection: close");
+            client.println();
+            
+            // turns the GPIOs on and off
+            if (header.indexOf("GET /5/on") >= 0) {
+              Serial.println("GPIO 5 on");
+              output5State = "on";
+              digitalWrite(output5, HIGH);
+            } else if (header.indexOf("GET /5/off") >= 0) {
+              Serial.println("GPIO 5 off");
+              output5State = "off";
+              digitalWrite(output5, LOW);
+            } else if (header.indexOf("GET /4/on") >= 0) {
+              Serial.println("GPIO 4 on");
+              output4State = "on";
+              digitalWrite(output4, HIGH);
+            } else if (header.indexOf("GET /4/off") >= 0) {
+              Serial.println("GPIO 4 off");
+              output4State = "off";
+              digitalWrite(output4, LOW);
+            }
+            
+            // Display the HTML web page
+            client.println("<!DOCTYPE html><html>");
+            client.println("<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">");
+            client.println("<link rel=\"icon\" href=\"data:,\">");
+            // CSS to style the on/off buttons 
+            // Feel free to change the background-color and font-size attributes to fit your preferences
+            client.println("<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}");
+            client.println(".button { background-color: #195B6A; border: none; color: white; padding: 16px 40px;");
+            client.println("text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}");
+            client.println(".button2 {background-color: #77878A;}</style></head>");
+            
+            // Web Page Heading
+            client.println("<body><h1>ESP8266 Web Server</h1>");
+            
+            // Display current state, and ON/OFF buttons for GPIO 5  
+            client.println("<p>GPIO 5 - State " + output5State + "</p>");
+            // If the output5State is off, it displays the ON button       
+            if (output5State=="off") {
+              client.println("<p><a href=\"/5/on\"><button class=\"button\">ON</button></a></p>");
+            } else {
+              client.println("<p><a href=\"/5/off\"><button class=\"button button2\">OFF</button></a></p>");
+            } 
+               
+            // Display current state, and ON/OFF buttons for GPIO 4  
+            client.println("<p>GPIO 4 - State " + output4State + "</p>");
+            // If the output4State is off, it displays the ON button       
+            if (output4State=="off") {
+              client.println("<p><a href=\"/4/on\"><button class=\"button\">ON</button></a></p>");
+            } else {
+              client.println("<p><a href=\"/4/off\"><button class=\"button button2\">OFF</button></a></p>");
+            }
+
+              client.println("<p> Hello World!</p>");
+              client.println("<p>");
+                client.println( year() );
+              client.println("</p>");
+              //day(), month(), year(), hour(), minute(), second()
+              client.println("<p>Day :: ");
+                client.println( day() );
+              client.println("</p>");
+
+              client.println("<p>Month ::");  
+                client.println( month() ); client.println("</p>");
+              client.println("<p>Year :: "); 
+                client.println( year() ); 
+              client.println("</p>");
+              client.println("<p>Hour :: "); 
+                client.println( hour() ); 
+              client.println("</p>");
+              client.println("<p>Minute :: "); 
+                client.println( minute() ); 
+              client.println("</p>");
+              client.println("<p>Second :: "); 
+                client.println( second() ); 
+              client.println("</p>");
+
+            client.println("</body></html>");
+            
+            // The HTTP response ends with another blank line
+            client.println();
+            // Break out of the while loop
+            break;
+          } else { // if you got a newline, then clear currentLine
+            currentLine = "";
+          }
+        } else if (c != '\r') {  // if you got anything else but a carriage return character,
+          currentLine += c;      // add it to the end of the currentLine
+        }
+      }
+    }
+    // Clear the header variable
+    header = "";
+    // Close the connection
+    client.stop();
+    Serial.println("Client disconnected.");
+    Serial.println("");
+  }
+
+
+// #### / WEBSERVER / CLIENT #################################################
+
   //digitalWrite(ESP_BUILTIN_LED, LOW);
   //delay(200);
   //digitalWrite(ESP_BUILTIN_LED, HIGH);
@@ -162,16 +315,75 @@ void loop() {
 //         FastLED.show();
 //   delay(2000);
 
-  rainbow();
+  //rainbow();
+  //fill_solid( leds6, NUM_LEDS, CHSV(HUE_RED,255,255));
   
    EVERY_N_MILLISECONDS( 100 ) { gHue++; } // slowly cycle the "base color" through the rainbow
-  FastLED.show();
+  
   // insert a delay to keep the framerate modest
   FastLED.delay(1000/FRAMES_PER_SECOND);
+
+
+// Timer functions to change color of LEDs
+  //EVERY_N_MILLISECONDS( 100 ) { 
+    //changeColor ();
+  //}
+
+  // if( minute() == 36 ){
+  //     //digitalWrite(6, HIGH);
+  //     fill_solid( leds6, NUM_LEDS, CHSV(HUE_BLUE,255,255));
+  // }
+  // if( minute() == 37 ){
+  //     fill_solid( leds6, NUM_LEDS, CHSV(0,0,0));
+  //     //digitalWrite(6, LOW);
+  //     //fill_solid( leds6, NUM_LEDS, CHSV(HUE_GREEN,255,255));
+  // }
+  //  if( minute() == 38 ){
+  //     //digitalWrite(6, LOW);
+  //     fill_solid( leds6, NUM_LEDS, CHSV(HUE_ORANGE,255,255));
+  // }
+  // if( minute() >= 43 ){
+  //     //digitalWrite(6, HIGH);
+  //     rainbow();
+  // }
+
+  if( minute() % 2 == 0){ // flip flop every other minute
+    fill_solid( leds6, NUM_LEDS, CHSV(HUE_BLUE,255,255));
+  }else{
+    //fill_solid( leds6, NUM_LEDS, CHSV(HUE_BLUE,255,255));
+    rainbow();
+  }
   
+  if ( ( hour() > 16 ) && ( hour() <= 22 ) ) { // Run between 6pm and 11pm
+      FastLED.show();
+  } else if ( ( hour() == 22 ) && ( minute() < 10 ) ) { // Go until 10min past 10pm
+    FastLED.show();
+  } else {
+      FastLED.clear();
+  }
+
 } // loop
 
 // ############## FUNCTIONS ################
+
+void changeColor(){
+  if( minute() == 7 ){
+      digitalWrite(6, HIGH);
+      fill_solid( leds6, NUM_LEDS, CHSV(HUE_BLUE,255,255));
+  }
+  if( minute() == 8 ){
+      fill_solid( leds6, NUM_LEDS, CHSV(0,0,0));
+      digitalWrite(6, LOW);
+  }
+   if( minute() == 9 ){
+      digitalWrite(6, LOW);
+      fill_solid( leds6, NUM_LEDS, CHSV(HUE_ORANGE,255,255));
+  }
+  if( minute() == 10 ){
+      digitalWrite(6, HIGH);
+      rainbow();
+  }
+}
 
 void rainbow() 
 {
@@ -231,7 +443,7 @@ void updateTimeFromServer() {
 // NPT server time retrieve code -------------------------------------------------------------------------------------------------------
 // Found at (and slightly munged) http://www.esp8266.com/viewtopic.php?p=18395 posted by user "nigelbe", it is all the info I have, thank you Nigel
 // Note that I've modified the dst function to (hopefully) get correct daylight savings time offset for USA
-#define localTimeOffset 21600UL // your localtime offset from UCT
+#define localTimeOffset 28800UL // your localtime offset from UCT in SECONDS :: PDT is 7 hours differnt than UTC - 
 WiFiUDP udp;
 unsigned int localPort = 2390; // local port to listen for UDP packets
 const char* timeServer = "us.pool.ntp.org"; // fall back to regional time server
@@ -331,4 +543,17 @@ time_t secondSunday(time_t t)
 time_t firstSunday(time_t t)
 {
   return nextSunday(t); //Once, first Sunday
+}
+
+////////////////////////////////////////////////////////
+//
+// PrintTime
+//
+// Serial Print time (DD/MM/YYYY - HH:MM:SS)
+//
+void PrintTime() {
+  char cTime[22];
+  
+    sprintf(cTime, "%02u/%02u/%4u - %02u:%02u:%02u", day(), month(), year(), hour(), minute(), second());
+    Serial.println(cTime);
 }
